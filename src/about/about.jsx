4cbfd4b97d40b2cab9ae1fab152ad27ece0a7258
unknown <sakshamverma798@gmail.com>
2024-12-27 15:04:28 +0530

    first commit by saksham

---
+++ src/about/about.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root2')).render(
+  <StrictMode>
+    <h1>Hi , this is about us page in vite react mpa</h1>
+  </StrictMode>,
+)
